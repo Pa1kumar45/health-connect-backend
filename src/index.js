b92429c80e6bc3cffd712cef66f8e5f4f7019b6f
Pa1kumar45 <palurupavankumar2005@gmail.com>
2025-10-29 20:19:46 +0530

    Update CORS to allow Netlify frontend

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://your-netlify-app-name.netlify.app', // Add your Netlify URL here
+  'https://super-puppy-3433f6.netlify.app',
 ];
 
 app.use(
